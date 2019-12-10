4d60d6d5554ec0e4129ea30d0cb9d8e6fd1d6a11
Erwin Laura Cuisara <watafackerwin2@gmail.com>
2019-12-09 21:49:06 -0400

    actualizando receptor

--- app/Http/Controllers/RequisitoController.php
+++ app/Http/Controllers/RequisitoController.php
@@ -171,6 +171,14 @@ class RequisitoController extends Controller
             $validados=\DB::select('SELECT validados.validado, users.id FROM validados , users , convocatorias WHERE validados.user_id=users.id AND validados.convocatoria_id=convocatorias.id AND convocatorias.id=?',[$idConvocatoria]);
             
             
-         return view('receptor.receptor')->with(compact('idConvocatoria', 'postulantes','validados','idUser'));
+         //return view('receptor.receptor')->with(compact('idConvocatoria', 'postulantes','validados','idUser'));
+
+         //para validar los items 
+        $contador=0;    
+        $secciones=\DB::select('SELECT seccions.* , convocatorias.titulo as convocatoriaTitulo FROM convocatorias,seccions, requerimientos WHERE convocatorias.id=requerimientos.convocatoria_id and seccions.requerimiento_id=requerimientos.id AND requerimientos.convocatoria_id=?  ORDER BY seccions.id',[$idConvocatoria]);
+        $subsecciones=\DB::select('SELECT subseccions.*  from subseccions, seccions, requerimientos ,convocatorias where subseccions.seccion_id=seccions.id and seccions.requerimiento_id=requerimientos.id and convocatorias.id=requerimientos.convocatoria_id and subseccions.seccion_id=? AND convocatorias.id=?',[$secciones[$contador]->id,$idConvocatoria]);
+        $items=\DB::select('SELECT items.* FROM items, seccions, subseccions, requerimientos,convocatorias WHERE items.subseccion_id=subseccions.id AND subseccions.seccion_id=seccions.id AND seccions.requerimiento_id=requerimientos.id AND requerimientos.convocatoria_id=convocatorias.id AND convocatorias.id=?',[$idConvocatoria]);
+
+        return view('receptor.evaluarItem')->with(compact('idConvocatoria', 'secciones','subsecciones','items','contador','idUser'));
     }
 }
